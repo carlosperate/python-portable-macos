7c997ca4e5b3a31337723552fd6e5d92c42343a8
carlosperate <carlosperate@users.noreply.github.com>
2018-05-09 21:22:32 +0100

    More echo in the build python script

--- build_python.sh
+++ build_python.sh
@@ -2,7 +2,7 @@
 
 alias large_echo='{ set +x; } 2> /dev/null; f(){ echo "#\n#\n# $1\n#\n#"; set -x; }; f'
 
-large_echo "Check OpenSSL installation path"
+large_echo "Check OpenSSL installation path and CWD"
 if brew ls --versions openssl > /dev/null; then
     OPENSSL_ROOT="$(brew --prefix openssl)"
     echo $OPENSSL_ROOT
@@ -10,16 +10,15 @@ else
     echo "Please install OpenSSL with brew: 'brew install openssl'"
     exit 1
 fi
-
-large_echo "Get the current working directory path"
 CURRENT_DIR="$PWD"
 echo $CURRENT_DIR
 
-large_echo "Download source"
+large_echo "Download and uncompress Python source"
 wget https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tgz
 tar -zxvf Python-3.6.5.tgz &> /dev/null
 
-large_echo "Build Python"
 cd Python-3.6.5
+large_echo "Configure Python"
 ./configure MACOSX_DEPLOYMENT_TARGET=10.9 CPPFLAGS="-I$OPENSSL_ROOT/include" LDFLAGS="-L$OPENSSL_ROOT/lib" --prefix="$CURRENT_DIR/python-portable"
+large_echo "Build Python"
 make altinstall
